8065ee441bf0d5896ad59b959a3a2105c3f3fd53
Zak <zakaribel@hotmail.com>
2024-12-17 11:30:11 +0100

    modified env var that caused process error (dotenv)

--- client/web/src/services/auth.js
+++ client/web/src/services/auth.js
@@ -1,6 +1,6 @@
 export const loginUser = async (email, password) => {
-    const response = await fetch(`${process.env.API_URL}/login`, {
-      method: 'POST',
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+    method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
     });
