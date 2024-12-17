95fceb1d94ea956caf6e30c49bcbeff50754458a
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-12-17 11:47:32 +0100

    Update server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
 import express from "express";
+import mariadb from "mariadb";
+
+const pool = mariadb.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    connectionLimit: 5
+});
+
+pool.getConnection().then((connection) => {
+    
+}).catch((error) => {
+    console.error({
+        "error": "Database Connection Error",
+        "message": error,
+    });
+    
+    pool.end();
+});
+
+import UsersRouter from "./routes/Users.js";
+import WorkspacesRouter from "./routes/Workspaces.js";
+import ChannelsRouter from "./routes/Channels.js";
 
 const app = express();
 
@@ -8,5 +31,9 @@ app.use(express.json());
 app.get("/", (request, response) => {
     response.sendFile("index.html", { root: "public" });
 });
+
+app.use("/users/", UsersRouter);
+app.use("/workspaces/", WorkspacesRouter);
+app.use("/channels/", ChannelsRouter);
 
 export default app;
---
+++ server/routes/Users.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+    
+} from "../controllers/Users.js";
+
+const router = express.Router();
+
+router.get("", (request, response) => {
+    
+});
+
+export default router;
