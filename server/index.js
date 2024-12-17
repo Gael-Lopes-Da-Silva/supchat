6f818a2dd2575405a7f7249e472d61f950969b2a
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-12-17 11:51:57 +0100

    Update server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import mariadb from "mariadb";
 
+import UsersRouter from "./routes/Users.js";
+import WorkspacesRouter from "./routes/Workspaces.js";
+import ChannelsRouter from "./routes/Channels.js";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const pool = mariadb.createPool({
     host: "localhost",
     user: "root",
@@ -9,7 +17,19 @@ const pool = mariadb.createPool({
 });
 
 pool.getConnection().then((connection) => {
+    console.log("Connected to database !");
     
+    app.get("/", (request, response) => {
+        response.sendFile("index.html", { root: "public" });
+    });
+    
+    app.use("/users/", UsersRouter);
+    app.use("/workspaces/", WorkspacesRouter);
+    app.use("/channels/", ChannelsRouter);
+
+    app.listen(5000, () =>
+        console.log("Server running on http://localhost:5000 !")
+    );
 }).catch((error) => {
     console.error({
         "error": "Database Connection Error",
@@ -18,22 +38,5 @@ pool.getConnection().then((connection) => {
     
     pool.end();
 });
-
-import UsersRouter from "./routes/Users.js";
-import WorkspacesRouter from "./routes/Workspaces.js";
-import ChannelsRouter from "./routes/Channels.js";
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", (request, response) => {
-    response.sendFile("index.html", { root: "public" });
-});
-
-app.use("/users/", UsersRouter);
-app.use("/workspaces/", WorkspacesRouter);
-app.use("/channels/", ChannelsRouter);
 
 export default app;
