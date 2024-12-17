99e5c8219c73f9275f29bc2a7877ddb55b1955bc
Zak <zakaribel@hotmail.com>
2024-12-17 12:05:51 +0100

    added query to get all users in users controller

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -1,3 +1,12 @@
 export async function createUser(request) {
 	
 }
+
+export const getUsers = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: "Erreur serveur", error: error.message });
+    }
+};
