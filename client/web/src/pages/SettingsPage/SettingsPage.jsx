55e08c6a8adbf401ca62661c6d3fab08fdd52100
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-01-09 20:54:40 +0100

    Added options

--- client/web/src/pages/SettingsPage/SettingsPage.jsx
+++ client/web/src/pages/SettingsPage/SettingsPage.jsx
@@ -2,6 +2,8 @@ import * as react from 'react';
 import * as reactdom from 'react-router-dom';
 import * as Fa from 'react-icons/fa6';
 
+import $ from 'jquery';
+
 import { authentificationHook } from '../../hooks/Authentification';
 
 import "./SettingsPage.css"
@@ -9,34 +11,100 @@ import "./SettingsPage.css"
 const SettingsPage = () => {
     const [user, setUser] = react.useState('');
 
+    const [guiVisibility, setGuiVisibility] = react.useState({
+        settingsMenu: {
+            myAccount: true,
+            appearance: false,
+        },
+    });
+
     const [theme, setTheme] = react.useState('light');
-    
+
     const navigate = reactdom.useNavigate();
-    
+
     react.useEffect(() => {
         authentificationHook();
 
         setUser(JSON.parse(localStorage.getItem('user')).data);
-        
+
         if (localStorage.getItem('gui.theme')) {
             setTheme(localStorage.getItem('gui.theme'));
         }
     }, []);
 
+    const updateGuiState = (key, value) => {
+        setGuiVisibility((prev) => ({ ...prev, [key]: value }));
+    };
+
     return (
         <div className={`settings-container ${theme}`}>
             <div className="settings-left">
-            </div>
-            <div className="settings-right">
-                <header>
-                    <div className='settings-right-header-buttons'>
+                <div className='settings-left-categorie'>
+                    <p>Paramètre utilisateur</p>
+                    <hr />
+                    <div className='settings-left-categorie-buttons'>
                         <button onClick={() => {
-                            navigate("/dashboard");
-                        }} title='Fermer les paramètres'><Fa.FaXmark /></button>
+                            updateGuiState("settingsMenu", {
+                                myAccount: true,
+                                appearance: false,
+                            });
+                        }}>Mon compte<span style={{ display: !guiVisibility.settingsMenu.myAccount ? "none" : "" }}></span></button>
                     </div>
-                </header>
-                <main></main>
+                </div>
+                <div className='settings-left-categorie'>
+                    <p>Paramètre de l'application</p>
+                    <hr />
+                    <div className='settings-left-categorie-buttons'>
+                        <button onClick={() => {
+                            updateGuiState("settingsMenu", {
+                                myAccount: false,
+                                appearance: true,
+                            });
+                        }}>Apparence<span style={{ display: !guiVisibility.settingsMenu.appearance ? "none" : "" }}></span></button>
+                    </div>
+                </div>
             </div>
+            {guiVisibility.settingsMenu.myAccount &&
+                <div className="settings-right">
+                    <header>
+                        <p>Mon compte</p>
+                        <div className='settings-right-header-buttons'>
+                            <button onClick={() => {
+                                navigate("/dashboard");
+                            }} title='Fermer les paramètres'><Fa.FaXmark /></button>
+                        </div>
+                    </header>
+                    <main></main>
+                </div>
+            }
+            {guiVisibility.settingsMenu.appearance &&
+                <div className="settings-right">
+                    <header>
+                        <p>Apparence</p>
+                        <div className='settings-right-header-buttons'>
+                            <button onClick={() => {
+                                navigate("/dashboard");
+                            }} title='Fermer les paramètres'><Fa.FaXmark /></button>
+                        </div>
+                    </header>
+                    <main>
+                        <div className='settings-right-main-theme'>
+                            <button className='light' style={{ borderColor: theme !== "light" ? "dark" : "#23A55A" }} onClick={(event) => {
+                                $(".settings-right-main-theme>button").css("border-color", "black");
+                                $(event.currentTarget).css("border-color", "#23A55A");
+                                setTheme("light");
+                                localStorage.setItem('gui.theme', "light");
+                            }}><Fa.FaCheck style={{ display: theme !== "light" ? "none" : "" }} /></button>
+                            <button className='dark' style={{ borderColor: theme !== "dark" ? "dark" : "#23A55A" }} onClick={(event) => {
+                                $(".settings-right-main-theme>button").css("border-color", "black");
+                                $(event.currentTarget).css("border-color", "#23A55A");
+                                setTheme("dark");
+                                localStorage.setItem('gui.theme', "dark");
+                            }}><Fa.FaCheck style={{ display: theme !== "dark" ? "none" : "" }} /></button>
+                        </div>
+                    </main>
+                </div>
+            }
         </div>
     );
 };
